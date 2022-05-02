073cba4bf94260c58a49f6e1075bcf8366120e77
RG-Dou <dourg0612@gmail.com>
2022-05-02 10:19:36 +0800

    script

--- samza-testbed/nexmark_scripts/run-effect.sh
+++ samza-testbed/nexmark_scripts/run-effect.sh
@@ -1,21 +1,21 @@
 RESULT="$(dirname $(dirname $(pwd)))/tools/results/effect"
 
-rm -rf $RESULT/$1
+#rm -rf $RESULT/$1
 mkdir $RESULT/$1
-
-#bash run-module.sh 1 'memory' $1
-#python -c 'import time; time.sleep(10)'
 
 bash run-module.sh 1 'both' $1
 python -c 'import time; time.sleep(10)'
 
-#bash run-module.sh 1 'CPU' $1
-#python -c 'import time; time.sleep(10)'
+bash run-module.sh 1 'memory' $1
+python -c 'import time; time.sleep(10)'
 
-#bash run-module.sh 1 'current' $1
-#python -c 'import time; time.sleep(10)'
+bash run-module.sh 1 'CPU' $1
+python -c 'import time; time.sleep(10)'
 
-#bash run-module.sh 1 'static' $1
+bash run-module.sh 1 'current' $1
+python -c 'import time; time.sleep(10)'
+
+bash run-module.sh 1 'static' $1
 
 cd python
 python draw_effect.py $RESULT/$1
